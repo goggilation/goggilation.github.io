ee9ff7c5c960c7cfcbe06fff8685db5a0829c7d3
Aleksander Djordjevic <aleksander.k.djordjevic@gmail.com>
2020-10-29 19:54:39 +0100

    added skeleton to feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,8 +3,9 @@ import CTATEXT from "../content/feedContent.json";
 import FeedCard from "./FeedCard";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Skeleton from "@material-ui/lab/Skeleton";
 import { useMediaQuery } from "@material-ui/core";
-import config from '../config'
+import config from "../config";
 
 let itemsList = [];
 
@@ -18,8 +19,58 @@ var base = Airtable.base(config.AIRTABLE_ID);
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    overflow: "hidden",
+  },
+  skeleton:{
+    backgroundColor: "rgba(255, 255, 255, 0.11)",
+  },
+  skeletonImage:{
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12
+  },
+  skeletonText:{
+    marginTop: -16
   },
 }));
+
+const showFeed = (items, classes, isMobile) => {
+  if (items.length > 0) {
+    return (
+      <Grid container className={classes.root} spacing={isMobile ? 4 : 6}>
+        {items.map((item, index) => {
+          return (
+            <FeedCard
+              key={index}
+              img={item.img[0].url}
+              projectName={item.projectName}
+              clientName={item.clientName}
+              description={item.description}
+              ctaText={CTATEXT.CTA[0][item.type]}
+              ctaLink={item.ctaLink}
+            />
+          );
+        })}
+      </Grid>
+    );
+  } else {
+    return (
+      <Grid container className={classes.root} spacing={isMobile ? 4 : 6}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Skeleton animation="wave" className={classes.skeleton + " " + classes.skeletonImage} variant="rect" height={343} />
+          <Skeleton animation="wave" className={classes.skeleton + " " + classes.skeletonText} variant="text" height={120} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Skeleton animation="wave" className={classes.skeleton + " " + classes.skeletonImage} variant="rect" height={343} />
+          <Skeleton animation="wave" className={classes.skeleton + " " + classes.skeletonText} variant="text" height={120} />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Skeleton animation="wave" className={classes.skeleton + " " + classes.skeletonImage} variant="rect" height={343} />
+          <Skeleton animation="wave" className={classes.skeleton + " " + classes.skeletonText} variant="text" height={120} />
+        </Grid>
+      </Grid>
+    );
+  }
+};
 
 const Feed = (props) => {
   const [items, setItems] = useState([]);
@@ -27,7 +78,7 @@ const Feed = (props) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("xs"));
 
-  useEffect( function effectFunction() {
+  useEffect(function effectFunction() {
     let isSubscribed = true;
     base("FeedItems")
       .select({
@@ -40,29 +91,13 @@ const Feed = (props) => {
         }
         records.forEach(function (record) {
           console.log(record.fields);
-          setItems(items => [...items, record.fields]);
+          setItems((items) => [...items, record.fields]);
         });
-      })
-      isSubscribed = false;
-    }, []);
+      });
+    isSubscribed = false;
+  }, []);
 
-  return (
-    <Grid container className={classes.root} spacing={isMobile ? 4 : 6}>
-      {items.map((item, index) => {
-        return (
-          <FeedCard
-            key={index}
-            img={item.img[0].url}
-            projectName={item.projectName}
-            clientName={item.clientName}
-            description={item.description}
-            ctaText={CTATEXT.CTA[0][item.type]}
-            ctaLink={item.ctaLink}
-          />
-        );
-      })}
-    </Grid>
-  );
+  return <>{showFeed(items, classes, isMobile)}</>;
 };
 
 export default Feed;
