e5b8fa37316f87778f7c62374fbdcf8a02363c21
Aleksander Djordjevic <aleksander.k.djordjevic@gmail.com>
2021-11-10 22:11:40 +0100

    set up start page with new design. Lets goo

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import '../styles/style.css';
 import Start from "./Start";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { Box } from "@material-ui/core";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 
@@ -21,17 +23,18 @@ const useStyles = makeStyles((theme) => ({
 
 
 const App = () => {
-  const classes = useStyles();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
   return (
-    <div className={classes.wrapper + " wrapper"}>
-    <Router>
-      <Navigation/>
-      <Switch>
-        <Route exact path="/" activeClassName="current" component={Start} />
-    </Switch>
-    <Footer/>
-    </Router>
-    </div>
+    <Box width={matches ? '768px' : 'auto'} m={matches ? "24px auto" : 2}>
+      <Router>
+        <Navigation/>
+        <Switch>
+          <Route exact path="/" activeClassName="current" component={Start} />
+      </Switch>
+      <Footer/>
+      </Router>
+    </Box>
   );
 };
 
--- src/components/Start.js
+++ src/components/Start.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import config from '../config';
+import { Grid, Box, List } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,25 +20,106 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Start = () => {
-  gtag('config', config.GA_MEAS_ID, {
-    'page_title' : 'Start Page',
-    'page_path': '/Start'});
+  // gtag('config', config.GA_MEAS_ID, {
+  //   'page_title' : 'Start Page',
+  //   'page_path': '/Start'});
   const classes = useStyles();
   return (
     <>
-      <Grid container spacing={4} direction="column">
-        <Grid item className={classes.helloSection}>
-          <h5>Hi! I'm Aleks 👋</h5>
-          <p className="body1">
-            I am a UI designer and hobbyist front end developer 🤓.
-          </p>
-          <p className="body1">
-            Currently developing my fatherly skills with my kids. Otherwise I'm
-            helping develop design systems at Volvo Group. Hit me up for
-            anything regarding design, sneakers or video games!
-          </p>
+      <Box spacing={0}>
+        <Grid container spacing={3}>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item>
+                <h4>Hi! I'm Aleks 👋</h4>
+              </Grid>
+              <Grid item>
+                <p className="body1">
+                  Pleasure to see you! This is my little space, I’m inkling to
+                  call it MySpace but I don’t wanna get sued! I’m currently
+                  working on perfecting design systems at Volvo Group in
+                  beautiful rainy Gothenburg.
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <h5>How I got here</h5>
+              </Grid>
+              <Grid item>
+                <p className="body1">
+                  From working at companies like Swedish National Radio and
+                  Volvo, I’ve learned not only the power of diverse opinions,
+                  but also the strength in being always being open to learn new
+                  things. It’s what brought me from a graphic designer out of
+                  school, to a shift as podcaster / part time host on radio and
+                  back to UI designer and front end developer. I find my spark by taking full ownership of whatever I do, and
+                  great pride in seing that thing to a a final state that
+                  everyone involved is happy to use.
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <h5>What I'm great at</h5>
+              </Grid>
+              <Grid item>
+                <p className="body1">
+                  <ul>
+                    <li style={{marginLeft: 24}}>UI Design</li>
+                    <li style={{marginLeft: 24}}>Strategic work</li>
+                    <li style={{marginLeft: 24}}>Figma</li>
+                  </ul>
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <h5>Other things I’m pretty good at</h5>
+              </Grid>
+              <Grid item>
+                <p className="body1">
+                  <ul>
+                    <li style={{marginLeft: 24}}>Front end development</li>
+                    <li style={{marginLeft: 24}}>Team management</li>
+                  </ul>
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <h5>How I look</h5>
+              </Grid>
+              <Grid item>
+                <p className="body1">
+                  Like this
+                  <img width="100%" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container spacing={1}>
+              <Grid item xs={12}>
+                <h5>Social contacts</h5>
+              </Grid>
+              <Grid item>
+                <p className="body1">
+                See some of my design work over at <span className="dribbble">Dribbble</span>. Catch my ramblings over at <span className="twitter">Twitter</span>. Or be professional with me at <span className="linkedin">LinkedIn</span>. And of course, you can always <span className="mailLink">email me</span> 📯
+                </p>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </>
   );
 };
