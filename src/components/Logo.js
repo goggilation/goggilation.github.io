6320f7466b13fcc4a321eaee3af4e19091cd174f
Aleksander Djordjevic <aleksander.k.djordjevic@gmail.com>
2022-04-12 08:49:52 +0200

    fixed logotype references

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import React, { useState, useRef } from 'react';
 const Logo = (props) => {
     return (
         <>
-            <img style={{float: "left"}} src={"src/images/logo/Transparent_" + props.size + ".png"} />
+            <img style={{float: "left"}} src={"src/logos/Transparent_" + props.size + ".png"} />
         </>
     );
 }
